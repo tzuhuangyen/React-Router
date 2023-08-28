f3f01d9de62de5f59c78b7c5af488ad43843981e
tzuhuangyen <912yan@gmail.com>
2023-08-28 16:55:21 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import "./App.css";
+import { HashRouter, NavLink, Route, Routes } from "react-router-dom";
+
+const Todo = () => {
+  return <p>這是 Todo 頁面</p>;
+};
+const Login = () => {
+  return <p>這是登入頁面</p>;
+};
+const Register = () => {
+  return <p>這是註冊頁面</p>;
+};
+
+function App() {
+  return (
+    <div className="container">
+      <HashRouter>
+        <div className="nav-link">
+          <NavLink to="/">
+            <p>回到首頁</p>
+          </NavLink>
+          <NavLink to="/register">
+            <p>註冊頁面</p>
+          </NavLink>
+          <NavLink to="/login">
+            <p>登入頁面</p>
+          </NavLink>
+          <NavLink to="/todo">
+            <p>Todo 頁面</p>
+          </NavLink>
+        </div>
+        {/* Routes, Route 練習區 */}
+        {/* 練習區 */}
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
